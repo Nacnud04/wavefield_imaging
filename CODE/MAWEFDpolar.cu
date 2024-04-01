9ba7b7967708e6e9898cd6cc490652ce1415705f
nacnud04 <duncanbyrne03@gmail.com>
2024-04-01 20:25:50 +0000

    dat.rsf is now the receiver data not the wavefield

--- CODE/MAWEFDpolar.cu
+++ CODE/MAWEFDpolar.cu
@@ -445,7 +445,7 @@ int main(int argc, char*argv[]) {
 
     fprintf(stderr,"\n");
 
-  /*  
+  
     cudaMemcpy(h_dd_pp, d_dd_pp, nsmp*nr*sizeof(float), cudaMemcpyDefault);
 
     sf_setn(ar, nr);
@@ -455,19 +455,7 @@ int main(int argc, char*argv[]) {
     sf_oaxa(Fdat, at, 2);
     sf_oaxa(Fdat, ar, 1);
 
-    sf_floatwrite(h_dd_pp, nsmp*nr*sizeof(float), Fdat);    
-    */
-
-    // UNCOMMENT TO EXTRACT WAVEFIELD AT FINAL TIMESTEP
-
-    cudaMemcpy(h_vel, d_po, nthpad*nrapad*sizeof(float), cudaMemcpyDefault);
-   
-    sf_setn(ara, nrapad);
-    sf_setn(ath, nthpad);
-    sf_oaxa(Fdat, ara, 1);
-    sf_oaxa(Fdat, ath, 2);
-
-    sf_floatwrite(h_vel, nthpad*nrapad*sizeof(float), Fdat);
+    sf_floatwrite(h_dd_pp, nsmp*nr, Fdat);    
 
     // FREE ALLOCATED MEMORY
     cudaFree(d_ww);
