88f87c9bd6e2a45d6497125fe8bf67ee4e08823c
nacnud04 <duncanbyrne03@gmail.com>
2025-04-21 15:33:23 -0600

    Added old functions for continuity

--- CODE/spher_kernels.cu
+++ CODE/spher_kernels.cu
@@ -167,6 +167,61 @@ __global__ void inject_single_source_3D(float *d_uu, float *d_ww,
 
 }
 
+__global__ void inject_sources_2D_const(float *d_po, float *d_ww,
+                                float *d_Sw00, float *d_Sw01, float *d_Sw10, float *d_Sw11,
+                                int *d_Sjx, int *d_Sjz,
+                                int it, int ns,
+                                int nxpad, int nzpad) {
+
+        int ss = threadIdx.x + blockIdx.x * blockDim.x;
+
+        float wa = d_ww[it];
+
+        if (ss < ns) {
+
+                int s_x = d_Sjx[ss];
+                int s_z = d_Sjz[ss];
+
+                d_po[ s_z      * nxpad + s_x    ] += wa * d_Sw00[ss];
+                d_po[(s_z + 1) * nxpad + s_x    ] += wa * d_Sw01[ss];
+                d_po[ s_z      * nxpad + s_x + 1] += wa * d_Sw10[ss];
+                d_po[(s_z + 1) * nxpad + s_x + 1] += wa * d_Sw11[ss];
+
+        }
+
+}
+
+__global__ void inject_sources_3D_const(float *d_po, float *d_ww, 
+                                float *d_Sw000, float *d_Sw001, float *d_Sw010, float *d_Sw011, 
+                                float *d_Sw100, float *d_Sw101, float *d_Sw110, float *d_Sw111, 
+                                int *d_Sjx, int *d_Sjy, int *d_Sjz, 
+                                int it, int ns,
+                                int nxpad, int nypad, int nzpad) {
+
+        int ss = threadIdx.x + blockIdx.x * blockDim.x;
+
+        float wa = d_ww[it];
+
+        if (ss < ns) {
+
+                int s_x = d_Sjx[ss];
+                int s_y = d_Sjy[ss];
+                int s_z = d_Sjz[ss];
+
+                int xz = nxpad * nzpad;
+
+                d_po[s_y*xz + s_z*nxpad         + s_x  ] += wa * d_Sw000[ss];
+                d_po[s_y*xz + (s_z+1)*nxpad     + s_x  ] += wa * d_Sw001[ss];
+                d_po[s_y*xz + s_z*nxpad         + s_x+1] += wa * d_Sw010[ss];
+                d_po[s_y*xz + (s_z+1)*nxpad     + s_x+1] += wa * d_Sw011[ss];
+                d_po[(s_y+1)*xz + s_z*nxpad     + s_x  ] += wa * d_Sw100[ss];
+                d_po[(s_y+1)*xz + (s_z+1)*nxpad + s_x  ] += wa * d_Sw101[ss];
+                d_po[(s_y+1)*xz + s_z*nxpad     + s_x+1] += wa * d_Sw110[ss];
+                d_po[(s_y+1)*xz + (s_z+1)*nxpad + s_x+1] += wa * d_Sw111[ss];
+
+        }
+}
+
 __global__ void inject_sources_2D(float *d_po, float *d_ww, float *d_vel,
         float *d_Sw00, float *d_Sw01, float *d_Sw10, float *d_Sw11,
         int *d_Sjx, int *d_Sjz,
